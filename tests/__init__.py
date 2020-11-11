d6654398863db20d5f736e1d3aa9bc531f5e35cb
Konrad Pagacz <konrad.pagacz@gmail.com>
2020-11-11 20:58:32 +0100

    Removed unnecessary tensorflow messages from testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,9 @@
 import logging
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+# Silence the tensorflow messages
+import os 
+import logging
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
